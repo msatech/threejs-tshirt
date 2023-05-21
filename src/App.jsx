329bcf18836bb7c34d495973aaf663f69c9ae961
msatech <deependrakumar049@gmail.com>
2023-05-22 00:19:04 +0530

    components and pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import Hello from './compoents/hello'
+import Canvas from "./canvas"
+import Customizer from "./pages/Customizer"
+import Home from "./pages/Home"
+
 function App() {
 
   return (
-    <>
-      <Hello />
-    </>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   )
 }
 
--- src/compoents/Hello.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Hello = () => {
-  return (
-    <div className='text-red-500'>Hello</div>
-  )
-}
-
-export default Hello
